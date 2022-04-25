634e46a218f497edf83d847a116a5341561e9472
dog14230pp <dog14230pp@gmail.com>
2022-04-25 12:10:43 +0800

    transform,py

--- zs_train_input_transform.py
+++ zs_train_input_transform.py
@@ -18,6 +18,7 @@ from torch.nn.parameter import Parameter
 
 from config import cfg
 from models import init_models_pairs
+import numpy as np
 
 torch.manual_seed(0)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -62,7 +63,7 @@ def compute_loss(model_outputs, labels):
 
 
 def accuracy_checking(
-    model_orig, model_p, trainloader, testloader, pg, device
+    model_orig, model_p, trainloader, testloader, pg, device, use_transform=False
 ):
     # For training data first:
     total_train = 0
@@ -74,9 +75,13 @@ def accuracy_checking(
     for x, y in trainloader:
         total_train += 1
         x, y = x.to(device), y.to(device)
-        x_adv = pg(x)
-        out_orig = model_orig(x_adv)
-        out_p = model_p(x_adv)
+        if use_transform:
+            x_adv = pg(x)
+            out_orig = model_orig(x_adv)
+            out_p = model_p(x_adv)
+        else:
+            out_orig = model_orig(x)
+            out_p = model_p(x)
         _, pred_orig = out_orig.max(1)
         _, pred_p = out_p.max(1)
         y = y.view(y.size(0))
@@ -88,9 +93,13 @@ def accuracy_checking(
     for x, y in testloader:
         total_test += 1
         x, y = x.to(device), y.to(device)
-        x_adv = pg(x)
-        out_orig = model_orig(x_adv)
-        out_p = model_p(x_adv)
+        if use_transform:
+            x_adv = pg(x)
+            out_orig = model_orig(x_adv)
+            out_p = model_p(x_adv)
+        else:
+            out_orig = model_orig(x)
+            out_p = model_p(x)
         _, pred_orig = out_orig.max(1)
         _, pred_p = out_p.max(1)
         y = y.view(y.size(0))
@@ -111,6 +120,8 @@ def accuracy_checking(
             accuracy_orig_test, accuracy_p_test
         )
     )
+
+    return accuracy_orig_train, accuracy_p_train, accuracy_orig_test, accuracy_p_test
 
 
 def transform_train(
@@ -185,53 +196,77 @@ def transform_train(
     )
     lb = cfg.lb  # Lambda
 
-    print(
-        "========== Start checking the accuracy "
-        "before applying input transform =========="
-    )
-    accuracy_checking(
-        model, model_perturbed, trainloader, testloader, Pg, device
-    )
+    #print(
+    #    "========== Start checking the accuracy "
+    #    "before applying input transform =========="
+    #)
+    #accuracy_checking(
+    #    model, model_perturbed, trainloader, testloader, Pg, device
+    #)
 
     for name, param in Pg.named_parameters():
         print("Param name: {}, grads is: {}".format(name, param.requires_grad))
 
     print(
         "========== Start training the parameter"
-        " of the input transform =========="
+        " of the input transform by using EOT attack =========="
     )
     for epoch in range(cfg.epochs):
         running_loss = 0
+        running_loss_forkeepclean = 0
         running_correct_orig = 0
         running_correct_p = 0
-        total = 0
+              
+        # For each epoch, we will use N perturbed model for training.
         for batch_id, (image, label) in enumerate(trainloader):
-            total += 1
+            total_loss = 0  
             image, label = image.to(device), label.to(device)
             image_adv = Pg(image)  # pylint: disable=E1102
-            out = model(image_adv)  # pylint: disable=E1102
-            out_biterror = model_perturbed(image_adv)  # pylint: disable=E1102
-            loss_orig, pred_orig = compute_loss(out, label)
-            loss_p, pred_p = compute_loss(out_biterror, label)
-            loss = loss_orig + lb * loss_p
+            for j in range(cfg.N):
+                # print("*** For using the {}-th perturbed model ***".format(j))
+                (model, checkpoint_epoch, model_perturbed, checkpoint_epoch_perturbed) = init_models_pairs( 
+                    arch, in_channels, precision, True, checkpoint_path, fl,  ber, pos, seed=j)
+                #model = nn.DataParallel(model)
+                #model_perturbed = nn.DataParallel(model_perturbed)
+                model, model_perturbed = model.to(device), model_perturbed.to(device)
+                # We only need to inference clean model for one time!
+                if j == 0: 
+                    model.eval()
+                    out = model(image_adv)  # pylint: disable=E1102
+                    loss_orig, pred_orig = compute_loss(out, label)
+                    running_correct_orig += torch.sum(pred_orig == label.data).item()
+                    running_loss_forkeepclean += loss_orig.item()
 
+                model_perturbed.eval()
+                out_biterror = model_perturbed(image_adv)  # pylint: disable=E1102
+                loss_p, pred_p = compute_loss(out_biterror, label)
+                total_loss += loss_p
+                # running_loss += loss_p.item()
+                running_correct_p += torch.sum(pred_p == label.data).item()
+            
+            total_loss /= cfg.N # Average the loss. Only perturbed loss is in this total_loss now!
+            total_loss = loss_orig + lb * total_loss
+            running_loss += total_loss.item()
             optimizer.zero_grad()
             # Pg.zero_grad()
-            loss.backward()
+            total_loss.backward()
             optimizer.step()
             lr_scheduler.step()
-            running_loss += loss.item()
-            running_correct_orig += torch.sum(pred_orig == label.data).item()
-            running_correct_p += torch.sum(pred_p == label.data).item()
+            #print(total_loss.item())
+            #print(Pg.P.grad)
+            
+            
 
-        accuracy_orig = running_correct_orig / (len(trainloader.dataset))
-        accuracy_p = running_correct_p / (len(trainloader.dataset))
+        accuracy_orig = running_correct_orig / len(trainloader.dataset)
+        accuracy_p = running_correct_p / (len(trainloader.dataset) * cfg.N)
         print(
-            "For epoch: {}, loss: {:.6f}, accuracy for clean model:"
-            "{:.5f}, accuracy for perturbed model: {:.5f}".format(
+            "For epoch: {}, loss: {:.6f}, accuracy for {} clean model:"
+            "{:.5f}, accuracy for {} perturbed model: {:.5f}".format(
                 epoch + 1,
-                running_loss / len(trainloader.dataset),
+                running_loss,
+                cfg.N,
                 accuracy_orig,
+                cfg.N,
                 accuracy_p,
             )
         )
@@ -245,6 +280,60 @@ def transform_train(
         #   lb *= 0.5
         #   print("Lambda value: ", lb)
 
+    print('========== Start checking the accuracy with different perturbed model ==========')
+    # Setting without input transformation
+    accuracy_orig_train_list = []
+    accuracy_p_train_list = []
+    accuracy_orig_test_list = []
+    accuracy_p_test_list = []
+
+    # Setting with input transformation
+    accuracy_orig_train_list_with_transformation = []
+    accuracy_p_train_list_with_transformation = []
+    accuracy_orig_test_list_with_transformation = []
+    accuracy_p_test_list_with_transformation = []
+
+    for i in range(1000, 1010):
+        print(' ********** For seed: {} ********** '.format(i))
+        (model, checkpoint_epoch, model_perturbed, checkpoint_epoch_perturbed) = init_models_pairs( 
+                    arch, in_channels, precision, True, checkpoint_path, fl,  ber, pos, seed=i)
+        model, model_perturbed = model.to(device), model_perturbed.to(device),
+        model.eval()
+        model_perturbed.eval()
+
+        # Without using transform
+        accuracy_orig_train, accuracy_p_train, accuracy_orig_test, accuracy_p_test = accuracy_checking(model, model_perturbed, trainloader, testloader, Pg, device, use_transform=False)
+        accuracy_orig_train_list.append(accuracy_orig_train)
+        accuracy_p_train_list.append(accuracy_p_train)
+        accuracy_orig_test_list.append(accuracy_orig_test)
+        accuracy_p_test_list.append(accuracy_p_test)
+
+        # With input transform
+        accuracy_orig_train, accuracy_p_train, accuracy_orig_test, accuracy_p_test = accuracy_checking(model, model_perturbed, trainloader, testloader, Pg, device, use_transform=True)
+        accuracy_orig_train_list_with_transformation.append(accuracy_orig_train)
+        accuracy_p_train_list_with_transformation.append(accuracy_p_train)
+        accuracy_orig_test_list_with_transformation.append(accuracy_orig_test)
+        accuracy_p_test_list_with_transformation.append(accuracy_p_test)
+  
+    # Without using transform
+    print('The average results without input transformation -> accuracy_orig_train: {:5f}, accuracy_p_train: {:5f}, accuracy_orig_test: {:5f}, accuracy_p_test: {:5f}'.format(
+      np.mean(accuracy_orig_train_list), np.mean(accuracy_p_train_list), np.mean(accuracy_orig_test_list), np.mean(accuracy_p_test_list)
+    ))
+    print('The average results without input transformation -> std_accuracy_orig_train: {:5f}, std_accuracy_p_train: {:5f}, std_accuracy_orig_test: {:5f}, std_accuracy_p_test: {:5f}'.format(
+      np.std(accuracy_orig_train_list), np.std(accuracy_p_train_list), np.std(accuracy_orig_test_list), np.std(accuracy_p_test_list)
+    ))
+
+    print()
+
+    # With input transform
+    print('The average results with input transformation -> accuracy_orig_train: {:5f}, accuracy_p_train: {:5f}, accuracy_orig_test: {:5f}, accuracy_p_test: {:5f}'.format(
+      np.mean(accuracy_orig_train_list_with_transformation), np.mean(accuracy_p_train_list_with_transformation), np.mean(accuracy_orig_test_list_with_transformation), np.mean(accuracy_p_test_list_with_transformation)
+    ))
+    print('The average results with input transformation -> std_accuracy_orig_train: {:5f}, std_accuracy_p_train: {:5f}, std_accuracy_orig_test: {:5f}, std_accuracy_p_test: {:5f}'.format(
+      np.std(accuracy_orig_train_list_with_transformation), np.std(accuracy_p_train_list_with_transformation), np.std(accuracy_orig_test_list_with_transformation), np.std(accuracy_p_test_list_with_transformation)
+    ))
+
+    '''
     print(
         "========== Start checking the accuracy "
         "after applying input transform =========="
@@ -252,3 +341,4 @@ def transform_train(
     accuracy_checking(
         model, model_perturbed, trainloader, testloader, Pg, device
     )
+    '''
